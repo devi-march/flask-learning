c41920a5d82b2d7f77e5b25a41094f6811df51be
devi-march <devi@stavir.com>
2024-06-10 09:15:14 +0530

    "edit employee"

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 # In-memory storage for employees
 employees = [
     {'id': 1, 'name': 'Santhosh'},
-    {'id': 2, 'name': 'Krishna Vamsi'},
+    {'id': 2, 'name': 'Krishna'},
     {'id': 3, 'name': 'Devi'},
-    {'id': 4, 'name': 'Jibu george'}
+    {'id': 3, 'name': 'Jibu George'},
 ]
 
 @app.route('/')
@@ -22,6 +22,15 @@ def add_employee():
         employees.append({'id': new_id, 'name': new_name})
         return redirect(url_for('index'))
     return render_template('add_employee.html')
+
+@app.route('/edit/<int:employee_id>', methods=['GET', 'POST'])
+def edit_employee(employee_id):
+    employee = next((emp for emp in employees if emp['id'] == employee_id), None)
+    if request.method == 'POST':
+        if employee:
+            employee['name'] = request.form['name']
+        return redirect(url_for('index'))
+    return render_template('edit_employee.html', employee=employee)
 
 if __name__ == '__main__':
     app.run(debug=True)
